eb161353752bbf3852c886680622260e8065d9e7
Roberto Perez Soriano <robertops@Robertos-MacBook-Pro.local>
2018-11-16 12:51:09 -0800

    Updates

---
+++ precache-manifest.66e7c60d74da3883e45dfd52cf0f7c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f7ec61aa34419e552f0",
+    "url": "/learning-react/static/js/runtime~main.5f7ec61a.js"
+  },
+  {
+    "revision": "5bed1493f7b56be14648",
+    "url": "/learning-react/static/js/main.5bed1493.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/learning-react/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "5bed1493f7b56be14648",
+    "url": "/learning-react/static/css/main.33e9d06a.chunk.css"
+  },
+  {
+    "revision": "4e8395689bfbc4f2f640573668e0c1dc",
+    "url": "/learning-react/index.html"
+  }
+];

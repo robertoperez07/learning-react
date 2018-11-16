a76f457721ab7fa21bf721b1954eed6f79667e7a
Roberto Perez Soriano <robertops@Robertos-MacBook-Pro.local>
2018-11-16 13:46:47 -0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// import App from './Api';
-import './index.css';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
